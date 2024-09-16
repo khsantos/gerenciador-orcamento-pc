cef669e1a5b9910ca84b8a3f390866455acc7f56
Kauã Heidemann Santos <104318673+khsantos@users.noreply.github.com>
2024-09-16 16:42:28 -0300

    add

---
+++ src/App.styles.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const Container = styled.div` 
+    
+`;
+
+export const Header = styled.div`
+    background: linear-gradient(180deg, rgba(255,255,255,1) 49%, rgba(236,236,236,1) 100%);
+    height: 150px;
+    text-align: center;
+`;
+
+export const HeaderText = styled.h1`
+    margin: 0;
+    padding: 0;
+    color: #000;
+    padding-top: 30px;
+`;
+
+export const Body = styled.div`
+    margin: auto;
+    max-width: 980px;
+    margin-bottom: 50px;
+`;
---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from "../@types/Item"
+
+export const items: Item[] = [
+    { date: new Date(2024, 9, 16), category: 'food', title: 'McDonalds', value: 32.15},
+    { date: new Date(2024, 9, 16), category: 'food', title: 'BurguerKing', value: 16.15},
+    { date: new Date(2024, 9, 16), category: 'rent', title: 'Apartamento', value: 1500},
+    { date: new Date(2024, 9, 16), category: 'salary', title: 'Salario Port', value: 6230},
+]
